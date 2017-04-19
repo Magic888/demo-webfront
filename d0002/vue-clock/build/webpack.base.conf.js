aa2c34e08fe2cba94e8f59ca8418a3143b3624cf
bobenut <33755383@qq.com>
2017-04-19 10:40:01 +0800

    created

---
+++ d0002/vue-clock/build/webpack.base.conf.js
@@ -0,0 +1,79 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  vue: {
+    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['last 2 versions']
+      }),
+      require('postcss-import'),
+      require('postcss-sass-extend'),
+      require('postcss-simple-vars'),
+      require('postcss-nested')//sass嵌套语法，其他的看最后一个单词就知道是干什么的了
+    ]
+  }
+}
---
+++ d0002/vue-clock/src/main.js
@@ -0,0 +1,42 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import store from './store'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './Components/Home'
+import Clocklist from './Components/Clocklist'
+import './static/css/reset.css'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/clocklist',
+    component: Clocklist
+  }
+]
+
+const router = new VueRouter({routes});
+
+new Vue({
+  store,
+  router,
+  ...App
+}).$mount('#app');
+
+
+//new Vue({
+//  el: '#app',
+//  router,
+//  template: '<App/>',
+//  components: { App }
+//})
